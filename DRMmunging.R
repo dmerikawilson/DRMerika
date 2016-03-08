7fa96f66c6ec6f8fd8df29528732e8a83d787570
whopper-sculder <wjhopper510@gmail.com>
2016-03-08 10:55:04 -0500

    wrote function to clean up the raw data. Recodes no_list, list, and studied variables into a new variable. class, and drops some redundant and not useuful ones

--- DRMmunging.R
+++ DRMmunging.R
@@ -1,1 +1,29 @@
-library(dplyr)
+DRM_data_setup <- function() {
+  stopifnot(require(dplyr))
+  recode_class <- function(studied, list) {
+    if (studied ==1) {
+      'target'
+    } else if (list == 'critical') {
+      'critical'
+    } else if (list == '') {
+      'lure'
+    } else {
+      'related'
+    }
+  }
+  fList <- list.files(path = file.path("data"), pattern = "*TestData.csv", full.names = TRUE)
+  data <- do.call(rbind, lapply(fList, read.csv))
+  names(data) <- tolower(names(data))
+  data <- data %>%  
+    mutate(word = tolower(word),
+           list = tolower(list),
+           rating = as.numeric(substring(new, 1,1)),
+           class = mapply(recode_class, studied, list),
+           list = replace(list, list == '', NA),
+           type = replace(type, class == "lure", NA_character_),
+           old_new = ifelse(new <= 3, "new", "old"),
+           acc = ifelse((studied == 1 & old_new == 'old') | (studied == 0 & old_new == 'new'), 1, 0)) %>%
+    select(subject = subnum, trial = trialnum, word, list, list_type = type, class, studied, rating, rt, old_new, acc)
+  
+  return(data)
+}
