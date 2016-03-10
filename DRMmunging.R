260b8996dab6137bb7dbc1149d7154a1a1789900
Merika Wilson <dmerikawilso@psych.umass.edu>
2016-03-10 14:12:39 -0500

    Fixed DRMmunging to use the correct column to determine old-new accuracy

--- DRMmunging.R
+++ DRMmunging.R
@@ -1,5 +1,5 @@
 DRM_data_setup <- function() {
-  stopifnot(require(dplyr))
+  stopifnot(require(dplyr)) #Makes sure package is installed and loaded
   recode_class <- function(studied, list) {
     if (studied ==1) {
       'target'
@@ -11,8 +11,8 @@ DRM_data_setup <- function() {
       'related'
     }
   }
-  
-  fList <- list.files(path = file.path("data"), pattern = "*TestData.csv", full.names = TRUE)
+  options(stringsAsFactors = FALSE)
+  fList <- list.files(path = file.path("data"), pattern = "*TestData.csv", full.names = TRUE) #Reading in files in "Data" folder with that extension
   data <- do.call(rbind, lapply(fList, read.csv))
   names(data) <- tolower(names(data))
   
@@ -22,7 +22,7 @@ DRM_data_setup <- function() {
            rating = as.numeric(substring(new, 1,1)),
            class = mapply(recode_class, studied, list),
            list = replace(list, list == '', NA),
-           old_new = ifelse(new <= 3, "new", "old"),
+           old_new = ifelse(rating <= 3, "new", "old"),
            acc = ifelse((studied == 1 & old_new == 'old') | (studied == 0 & old_new == 'new'), 1, 0)) %>%
     select(subject = subnum, trial = trialnum, word, list, list_type = type, class, studied, rating, rt, old_new, acc)
   
