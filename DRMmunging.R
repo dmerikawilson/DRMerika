cbc14832d808a52c7dfd53684773c51e23b36118
Merika Wilson <dmerikawilso@psych.umass.edu>
2016-03-17 13:39:18 -0400

    added list length variable to DRM data

--- DRMmunging.R
+++ DRMmunging.R
@@ -16,6 +16,15 @@ DRM_data_setup <- function() {
   data <- do.call(rbind, lapply(fList, read.csv))
   names(data) <- tolower(names(data))
   
+  fList <- list.files(path = file.path("data"), pattern = "*StudyData.csv", full.names = TRUE) #Reading in files in "Data" folder with that extension
+  studyData <- do.call(rbind, lapply(fList, read.csv))
+  names(studyData) <- tolower(names(studyData))
+  list_length <- studyData %>%
+    group_by(subnum, list) %>%
+    summarise(list_length = n()) %>%
+    mutate(list = ifelse(list == "", NA, tolower(list)),
+           list_length = replace(list_length, is.na(list), 0))
+  
   data <- data %>%  
     mutate(word = tolower(word),
            list = tolower(list),
@@ -24,7 +33,9 @@ DRM_data_setup <- function() {
            list = replace(list, list == '', NA),
            old_new = ifelse(rating <= 3, "new", "old"),
            acc = ifelse((studied == 1 & old_new == 'old') | (studied == 0 & old_new == 'new'), 1, 0)) %>%
-    select(subject = subnum, trial = trialnum, word, list, list_type = type, class, studied, rating, rt, old_new, acc)
+    left_join(y=list_length, by=c("subnum","list")) %>%
+    select(subject = subnum, trial = trialnum, word, list, list_type = type,
+           list_length, class, studied, rating, rt, old_new, acc)
   
   return(data)
 }
